d4eb30c32da1a28723a94c9780996fa7725fca73
Daniel Ebonine <ebonined@gmail.com>
2022-06-23 19:24:16 +0100

    Final change in `__init__.py`

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -37,9 +37,6 @@ def create_app(test_config=None):
 
     CORS(app, resources={r'/': {'origins': '*'}})
 
-    """
-    @TODO: Use the after_request decorator to set Access-Control-Allow
-    """
     @app.after_request
     def after_request(response):
         response.headers.add('Access-Control-Allow-Headers',
